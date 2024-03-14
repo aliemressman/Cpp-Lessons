c8eb0219bd96a73e9552a00c2bb37e0ff567b5db
aliemressman <145591935+aliemressman@users.noreply.github.com>
2024-03-14 09:25:28 +0300

    How to define a variable ?

---
+++ variable definition rules/lesson1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 15, y = 65, z = 21;
+
+    cout<<"x in value : "<<x<<"\n";
+    cout<<"y in value : "<<y<<"\n";
+    cout<<"z in value : "<<z<<"\n";
+
+    x=y=z=130;
+
+    cout<<"x in value : "<<x<<"\n";
+    cout<<"y in value : "<<y<<"\n";
+    cout<<"z in value : "<<z<<"\n";
+
+    string myName = "Ali Emre";
+    string mySurName = "Sisman";
+
+    cout<<"My name is "<<myName<<"\n"<<"My surname is "<<mySurName;
+    return 0;
+
+}
