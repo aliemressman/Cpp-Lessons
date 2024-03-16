151ee9a84dfba60ea14f3a79df9d71f9c181ddaf
aliemressman <145591935+aliemressman@users.noreply.github.com>
2024-03-16 11:42:54 +0300

    7-Conditional Statements with If Else

---
+++ 7-Conditional Statements with If Else/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int number1, number2;
+    cout<<"Enter the number";
+    cin>>number1;
+
+    cout<<"Enter the number";
+    cin>>number2;
+
+    if(number1 > number2){
+        cout<<"number1 > number2\n";
+        cout<<"Toplam degerleri: "<< number1 + number2;
+    }
+    else if(number1 == number2){
+        cout<<"number1 == number2 \n";
+        cout<<"carpim degerleri : "<< number1 * number2;
+    }
+    else{
+        cout<<"number1 < number2\n";
+        cout<<"Bolumunden kalan degerleri : "<< number2 % number1;
+    }
+}
