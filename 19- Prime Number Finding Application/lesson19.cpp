ff7a63931a81b7794891df123f2ab14b6c1e425c
aliemressman <145591935+aliemressman@users.noreply.github.com>
2024-03-21 18:55:32 +0300

    19- Prime Number Finding Application Next
    Tekrar tekrar kullanıcıdan değer almak.

--- 19- Prime Number Finding Application/lesson19.cpp
+++ 19- Prime Number Finding Application/lesson19.cpp
@@ -6,23 +6,32 @@ int main() {
     // Sayinin asal olup olmadigini anlamamiz icin sayinin 
     // yarisina bolup o degere kadarki sayilarla bolup kontrol edebiliriz.
 
-    int number, result;
-    string control;
+    int number, result, stop;
+    int control = 1;
 
-    cout << "Bir deger giriniz: ";
+    do {
+    cout << "\nBir deger giriniz:\n";
     cin >> number;
-    result = (sqrt(number));
+    if(number < 1) {
+       cout<<"Lutfen yuksek degerde sayi giriniz.";
+       continue;
+    }
+    if(number == 1)
+        break;
 
+    result = (sqrt(number));
     for(int i = 2; i <= result; i++){
        if(number % i == 0){
-            control = "asaldir";
-               break;
-       }
+            control = 2;
+            break;
+        }else 
+           control = 1; 
     } 
-     if(control == "asaldir"){
-            cout << "Sayi asal degildir.";
-        }
-        else{
-            cout << "Sayi asaldir.";
-        }
+    if(control == 2)
+        cout << "Sayi asal degildir.";
+    else 
+        cout << "Sayi asaldir.";
+
+    }  while(number != 1);
+      return 0;
 }
