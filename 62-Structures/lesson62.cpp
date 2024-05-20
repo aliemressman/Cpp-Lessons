8c12c8d7be9ff788ce82fea8fcd23fdc24da37d8
Ali Emre Şişman <145591935+aliemressman@users.noreply.github.com>
2024-05-20 17:23:11 +0300

    62-Structures(with ptr)

--- 62-Structures/lesson62.cpp
+++ 62-Structures/lesson62.cpp
@@ -9,17 +9,12 @@ struct Car {
 
 int main() {
     Car car1 = {"Bmw", 25, "X5"};
-    Car car2 = {"Porsh", 5, "Taycan"};
+    
+    Car* ptr = &car1;
 
-    cout << car1.carName << endl;
-    cout << car1.carAge << endl;
-    cout << car1.carModel << endl;
-    
-    cout << "--------------------------" << endl;
-    
-    cout << car2.carName << endl;
-    cout << car2.carAge << endl;
-    cout << car2.carModel << endl;
+    cout <<  ptr->carName << endl;
+    cout << ptr->carAge << endl;
+    cout << ptr->carModel << endl;
 
   return 0;   
 }
